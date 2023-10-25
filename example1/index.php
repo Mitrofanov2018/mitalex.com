799c8365820588b424472cd594d2b23ce2f17cc6
Mitrofanov2018 <mitrofan7878@mail.ru>
2023-10-25 14:04:51 +0300

    3 commit

---
+++ example1/index.php
@@ -0,0 +1,4 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Страница с примером");
+?>Привет мир<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
